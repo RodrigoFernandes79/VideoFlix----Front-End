094a6b521a34133e9205a8ec387ee661e69e2ddd
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-12-10 10:39:46 -0300

    Redirecionando a rota ao autenticar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 	{path:'',redirectTo:'home',pathMatch:'full'},
-	{path:'home',loadChildren:()=> import('./home/home.module').then(m=> m.HomeModule)}
+	{path:'home',loadChildren:()=> import('./home/home.module').then((m)=> m.HomeModule)},
+	{path:'videos', loadChildren:()=> import('./videos/videos.module').then((m)=>m.VideosModule)}
 ];
 
 @NgModule({
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
@@ -11,7 +12,8 @@ export class LoginComponent implements OnInit {
 	email = "";
 	senha = "";
 
-  constructor(private autencicacaoService: AutenticacaoService) { }
+  constructor(private autencicacaoService: AutenticacaoService,
+		private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -20,6 +22,7 @@ export class LoginComponent implements OnInit {
 	this.autencicacaoService.autenticar(this.email,this.senha)
 	.subscribe((resposta)=>{
 		console.log('Sucesso! '+resposta)
+		this.router.navigate(['videos']);
 	},
 	(error)=>{
 		alert('Erro de Autenticação. Email e/ou Senha inválidos.');
