b8e4e66a2fff9769c1dcd0255059f4609b7daa90
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2023-01-15 18:57:43 -0300

    RxJS e seu Subject para fazer pipe de tempo de filtragem

--- src/app/videos/video-list/video-list.component.ts
+++ src/app/videos/video-list/video-list.component.ts
@@ -1,39 +1,48 @@
-import { Video } from './../../models/video';
-import { CategoriaService } from './../../services/categoria.service';
-
-import { Component, OnInit } from '@angular/core';
-
-import { VideoFlixService } from 'src/app/services/video-flix.service';
-import { Categoria } from 'src/app/models/categoria';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+import { Video } from './../../models/video';
+import { CategoriaService } from './../../services/categoria.service';
+import { Categoria } from 'src/app/models/categoria';
+
 
 @Component({
   selector: 'app-video-list',
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.scss']
 })
-export class VideoListComponent implements OnInit {
+export class VideoListComponent implements OnInit, OnDestroy {
 
-  constructor(private videoFlixService:VideoFlixService, private catService : CategoriaService,
+  constructor(private catService : CategoriaService,
 		private sanitizer: DomSanitizer, private activatedRoute: ActivatedRoute) {}
 
- videos:Video[]=[]
-categorias:Categoria[]=[]
-filter:string = ''
+		videos:Video[]=[]
+		categorias:Categoria[]=[]
+		filter:string = ''
+debounce: Subject<string>= new Subject<string>()
 
-ngAfterViewInit() {
 
-}
 
-	ngOnInit(): void {
-		this.videos = this.activatedRoute.snapshot.data.videos['content']
-		this.videos.forEach(vid =>
-			vid.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(vid.url))
+ngOnInit(): void {
+
+	this.videos = this.activatedRoute.snapshot.data.videos['content']
+	this.videos.forEach(vid =>
+		vid.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(vid.url))
+
+		this.debounce
+		.pipe(debounceTime(400)) //Somente procura o filtro apos 400 milisegundos
+		.subscribe(filter => this.filter = filter)
 
 
 	}
-//-> deixando de usar o método para iniciar a aplicação com o resolve diretamente no NgOnInit
+
+	ngOnDestroy(): void {
+		this.debounce.unsubscribe() //encerrar o consumo do filtro do componente VideoListComponent
+	}
+	//-> deixando de usar o método para iniciar a aplicação com o resolve diretamente no NgOnInit
 // 	listarVideos():void{
 // 		this.videoFlixService.buscarVideos()
 // 		.subscribe((resposta) =>{
