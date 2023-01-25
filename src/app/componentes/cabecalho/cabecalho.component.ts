e261d9bb0fbccdd1b5bd4dcd91c3a1b5433869a3
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2023-01-25 20:21:54 -0300

    Inserindo botão para adicionar video no cabeçalho ,caso o usuário esteja logado

--- src/app/componentes/cabecalho/cabecalho.component.ts
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { MenuItem } from 'primeng-lts/api';
 
 
 @Component({
@@ -9,13 +10,14 @@ import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
   styleUrls: ['./cabecalho.component.scss']
 })
 export class CabecalhoComponent implements OnInit {
-
+	items: MenuItem[]= []
 	user$ = this.usuarioService.retornaUsuario()
 
   constructor(private usuarioService: UsuarioService, private router: Router) { }
 
   ngOnInit(): void {
 console.log(this.usuarioService.retornaUsuario())
+this.items = []
 
   }
 logout(){
--- src/app/componentes/cabecalho/cabecalho.module.ts
+++ src/app/componentes/cabecalho/cabecalho.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabecalhoComponent } from './cabecalho.component';
 import {MenubarModule} from 'primeng-lts/menubar';
-
+import {ButtonModule} from 'primeng-lts/button';
+import {MenuItem} from 'primeng-lts/api';
 
 @NgModule({
   declarations: [CabecalhoComponent],
   imports: [
     CommonModule,
 		RouterModule,
-		MenubarModule
+		MenubarModule,
+		ButtonModule,
+
   ],
 	exports:[CabecalhoComponent]
 
