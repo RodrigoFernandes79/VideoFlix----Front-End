88c86afffce80427da0fee030000dc48981eb86d
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2023-01-28 15:41:05 -0300

    Finalizando Validação com FormGroup nos campos titulo,descrição e url do novo-usuário

--- src/app/videos/novo-video/novo-video.component.ts
+++ src/app/videos/novo-video/novo-video.component.ts
@@ -2,6 +2,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component ,  Input,  OnInit } from '@angular/core';
 
 import { VideoFlixService } from 'src/app/services/video-flix.service';
+import { Video } from 'src/app/models/video';
 
 
 
@@ -17,13 +18,15 @@ export class NovoVideoComponent implements OnInit {
 displayPosition: boolean;
  formGroup:FormGroup
   position: string;
+video:Video = new Video()
+myPattern= /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/
 
   constructor( private formBuilder: FormBuilder, private videoFlixService :VideoFlixService ) {
 		this.formGroup = this.formBuilder.group({
 			titulo: ['',[Validators.required]],
 			descricao: ['',[Validators.required]],
-			url: ['',[Validators.required]],
-			categoria: [''],
+			url: ['',[Validators.required, Validators.pattern(this.myPattern)]],
+			categoria: [{id:1}],
 
 		})
 
@@ -42,11 +45,15 @@ displayPosition: boolean;
 	criarVideo() {
 		this.videoFlixService.inserirVideo(this.formGroup.value)
 		.subscribe(resposta =>{
+			this.video = resposta
 			console.log(resposta)
-
+			this.displayPosition = false;
+			this.formGroup.reset()
 		})
-		this.formGroup.reset()
-
+		}
+		cancelar() {
+			this.displayPosition = false;
+			this.formGroup.reset()
 
 		}
 }
